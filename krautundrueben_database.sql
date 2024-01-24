1903ad43e8313e25a1fc04200f3b1bbddea9b380
JonasRingeis <jonas.ringeis@otto.de>
2024-01-24 09:11:01 +0100

    Create krautundrueben_database.sql

---
+++ krautundrueben_database.sql
@@ -0,0 +1,173 @@
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: krautundrueben
+-- ------------------------------------------------------
+-- Server version	8.0.36
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `bestellung`
+--
+
+DROP TABLE IF EXISTS `bestellung`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bestellung` (
+  `BESTELLNR` int NOT NULL AUTO_INCREMENT,
+  `KUNDENNR` int DEFAULT NULL,
+  `BESTELLDATUM` date DEFAULT NULL,
+  `RECHNUNGSBETRAG` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`BESTELLNR`),
+  KEY `KUNDENNR` (`KUNDENNR`),
+  CONSTRAINT `bestellung_ibfk_1` FOREIGN KEY (`KUNDENNR`) REFERENCES `kunde` (`KUNDENNR`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bestellung`
+--
+
+LOCK TABLES `bestellung` WRITE;
+/*!40000 ALTER TABLE `bestellung` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bestellung` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `bestellungzutat`
+--
+
+DROP TABLE IF EXISTS `bestellungzutat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `bestellungzutat` (
+  `BESTELLNR` int NOT NULL,
+  `ZUTATENNR` int NOT NULL,
+  `MENGE` int DEFAULT NULL,
+  PRIMARY KEY (`BESTELLNR`,`ZUTATENNR`),
+  KEY `ZUTATENNR` (`ZUTATENNR`),
+  CONSTRAINT `bestellungzutat_ibfk_1` FOREIGN KEY (`BESTELLNR`) REFERENCES `bestellung` (`BESTELLNR`),
+  CONSTRAINT `bestellungzutat_ibfk_2` FOREIGN KEY (`ZUTATENNR`) REFERENCES `zutat` (`ZUTATENNR`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `bestellungzutat`
+--
+
+LOCK TABLES `bestellungzutat` WRITE;
+/*!40000 ALTER TABLE `bestellungzutat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `bestellungzutat` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `kunde`
+--
+
+DROP TABLE IF EXISTS `kunde`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `kunde` (
+  `KUNDENNR` int NOT NULL,
+  `NACHNAME` varchar(50) DEFAULT NULL,
+  `VORNAME` varchar(50) DEFAULT NULL,
+  `GEBURTSDATUM` date DEFAULT NULL,
+  `STRASSE` varchar(50) DEFAULT NULL,
+  `HAUSNR` varchar(6) DEFAULT NULL,
+  `PLZ` varchar(5) DEFAULT NULL,
+  `ORT` varchar(50) DEFAULT NULL,
+  `TELEFON` varchar(25) DEFAULT NULL,
+  `EMAIL` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`KUNDENNR`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `kunde`
+--
+
+LOCK TABLES `kunde` WRITE;
+/*!40000 ALTER TABLE `kunde` DISABLE KEYS */;
+/*!40000 ALTER TABLE `kunde` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `lieferant`
+--
+
+DROP TABLE IF EXISTS `lieferant`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `lieferant` (
+  `LIEFERANTENNR` int NOT NULL,
+  `LIEFERANTENNAME` varchar(50) DEFAULT NULL,
+  `STRASSE` varchar(50) DEFAULT NULL,
+  `HAUSNR` varchar(6) DEFAULT NULL,
+  `PLZ` varchar(5) DEFAULT NULL,
+  `ORT` varchar(50) DEFAULT NULL,
+  `TELEFON` varchar(25) DEFAULT NULL,
+  `EMAIL` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`LIEFERANTENNR`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lieferant`
+--
+
+LOCK TABLES `lieferant` WRITE;
+/*!40000 ALTER TABLE `lieferant` DISABLE KEYS */;
+/*!40000 ALTER TABLE `lieferant` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `zutat`
+--
+
+DROP TABLE IF EXISTS `zutat`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `zutat` (
+  `ZUTATENNR` int NOT NULL,
+  `BEZEICHNUNG` varchar(50) DEFAULT NULL,
+  `EINHEIT` varchar(25) DEFAULT NULL,
+  `NETTOPREIS` decimal(10,2) DEFAULT NULL,
+  `BESTAND` int DEFAULT NULL,
+  `LIEFERANT` int DEFAULT NULL,
+  `KALORIEN` int DEFAULT NULL,
+  `KOHLENHYDRATE` decimal(10,2) DEFAULT NULL,
+  `PROTEIN` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`ZUTATENNR`),
+  KEY `LIEFERANT` (`LIEFERANT`),
+  CONSTRAINT `zutat_ibfk_1` FOREIGN KEY (`LIEFERANT`) REFERENCES `lieferant` (`LIEFERANTENNR`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `zutat`
+--
+
+LOCK TABLES `zutat` WRITE;
+/*!40000 ALTER TABLE `zutat` DISABLE KEYS */;
+/*!40000 ALTER TABLE `zutat` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-01-24  9:09:21

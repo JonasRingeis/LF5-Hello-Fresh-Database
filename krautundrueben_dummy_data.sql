7086063963901840f4415206f6a28cf363eb2ab0
tjarkfischer <tjark.fischer@otto.com>
2024-01-26 14:00:01 +0100

    Test Data Complete

--- krautundrueben_dummy_data.sql
+++ krautundrueben_dummy_data.sql
@@ -37,16 +37,6 @@ INSERT INTO NÄHRWERTE (NÄHRWERTE_ID, KALORIEN, KOHLENHYDRATE, PROTEINE, BALLAS
                                                                                                             (9, 320, 80, 0, 0, 0, 2), 
                                                                                                             (10, 40, 10, 1, 2, 0, 0);
 
--- HALTUNGSFORM
-INSERT INTO HALTUNGSFORM (HALTUNGSFORM_ID, ART) VALUES (1, "Haltungsform 1, Stallhaltung"), 
-                                                        (2, "Haltungsform 2, StallhaltungPlus"),
-                                                        (3, "Haltungsform 3, Außenklima"),
-                                                        (4, "Haltungsform 4, Premium"),
-                                                        (5, "Kleingruppen");
-                                                        (6, "Bodenhaltung");
-                                                        (7, "Freilandhaltung");
-                                                        (8, "Öke-Haltung");
-
 -- ZUTAT
 INSERT INTO ZUTAT (ZUTAT_ID, BEZEICHNUNG, EINHEIT, NETTOPREIS, BESTAND, LIEFERANTEN_ID, NÄHRWERTE_ID, HALTUNGSFORM_ID) VALUES (1, 'Bio-Rinderhackfleisch', 'g', 3.99, 100, 1, 1, 4), 
                                                                                                                             (2, 'Frische Hühnereier', 'Stück', 0.25, 500, 2, 2, 4), 
@@ -57,10 +47,19 @@ INSERT INTO ZUTAT (ZUTAT_ID, BEZEICHNUNG, EINHEIT, NETTOPREIS, BESTAND, LIEFERAN
                                                                                                                             (7, 'Bio-Haferflocken', 'g', 1.49, 1000, 7, 7, 0), 
                                                                                                                             (8, 'Frische Paprika', 'Stück', 0.75, 800, 8, 8, 0),
                                                                                                                             (9, 'Bio-Honig', 'g', 3.49, 300, 9, 9, 0), 
-                                                                                                                            (10, 'Frische Zwiebeln', 'Stück', 0.30, 1200, 10, 10, 0)
+                                                                                                                            (10, 'Frische Zwiebeln', 'Stück', 0.30, 1200, 10, 10, 0);
 
 -- ZUTAT_NÄHRWERTE
-INSERT INTO ZUTAT_NÄHRWERTE (NÄHRWERTE_ID, ZUTAT_ID) VALUES (1, 1), (2, 2), (3, 3), ( 4, 4), (5, 5) (6, 6), (7, 7), (8, 8), (9, 9), (10, 10);
+INSERT INTO ZUTAT_NÄHRWERTE (NÄHRWERTE_ID, ZUTAT_ID) VALUES (1, 1), 
+															(2, 2),
+                                                            (3, 3),
+                                                            (4, 4),
+                                                            (5, 5),
+                                                            (6, 6),
+                                                            (7, 7),
+                                                            (8, 8),
+                                                            (9, 9),
+                                                            (10, 10);
 
 -- BESTELLUNG
 INSERT INTO BESTELLUNG (BESTELL_ID, KUNDEN_ID, BESTELLDATUM, RECHNUNGSBETRAG) VALUES (1, 1, '2022-01-15', 50.99), 
@@ -72,27 +71,35 @@ INSERT INTO BESTELLUNG (BESTELL_ID, KUNDEN_ID, BESTELLDATUM, RECHNUNGSBETRAG) VA
                                                                                     (7, 3, '2022-07-17', 42.30), 
                                                                                     (8, 2, '2022-08-22', 65.25), 
                                                                                     (9, 4, '2022-09-14', 30.00), 
-                                                                                    (10, 1, '2022-10-30', 55.99);
+                                                                                    (10, 1, '2022-10-30', 55.99),
+                                                                                    (11, 4, '2022-09-14', 9.99), 
+                                                                                    (12, 1, '2022-10-30', 14.99),
+                                                                                    (13, 1, '2022-10-30', 10.00),
+                                                                                    (14, 1, '2022-10-30', 10.00);
 
 -- BESTELLUNG_ZUTAT
 INSERT INTO BESTELLUNG_ZUTAT (BESTELL_ID, ZUTAT_ID, MENGE) VALUES (1, 1, 2), (1, 5, 5), (1, 9, 1), (1, 10, 1), (1, 7, 1), (1, 3, 1), (1, 6, 1), (1, 8, 1), (1, 2, 1),
                                                                 (2, 4, 3), (2, 7, 2), (2, 10, 1), (2, 1, 1), (2, 3, 1), (2, 6, 1), (2, 9, 1), (2, 5, 1), (2, 8, 1), (2, 2, 1),
                                                                 (3, 3, 4), (3, 6, 3), (3, 9, 2), (3, 2, 1), (3, 5, 1), (3, 8, 1), (3, 1, 1), (3, 4, 1), (3, 7, 1), (3, 10, 1),
                                                               (4, 10, 6), (4, 7, 4), (4, 4, 3), (4, 1, 2), (4, 8, 1), (4, 2, 1), (4, 3, 1), (4, 5, 1), (4, 6, 1), (4, 9, 1),
-                                                              (5, 5, 10), (5, 3, 5), (5, 8, 2), (5, 1, 1), (5, 4, 1), (5, 6, 1), (5, 7, 1), (5, 9, 1), (5, 10, 1), (5, 2, 1)
+                                                              (5, 5, 10), (5, 3, 5), (5, 8, 2), (5, 1, 1), (5, 4, 1), (5, 6, 1), (5, 7, 1), (5, 9, 1), (5, 10, 1), (5, 2, 1),
                                                               (6, 2, 7), (6, 9, 6), (6, 6, 5), (6, 3, 2), (6, 5, 1), (6, 8, 1), (6, 1, 1), (6, 4, 1), (6, 7, 1), (6, 10, 1),
                                                               (7, 6, 8), (7, 1, 4), (7, 4, 3), (7, 7, 2), (7, 10, 2), (7, 2, 1), (7, 3, 1), (7, 5, 1), (7, 8, 1), (7, 9, 1),
                                                               (8, 8, 10), (8, 5, 5), (8, 2, 3), (8, 9, 2), (8, 1, 1), (8, 4, 1), (8, 6, 1), (8, 7, 1), (8, 10, 1), (8, 3, 1),
                                                               (9, 3, 8), (9, 6, 6), (9, 9, 4), (9, 1, 2), (9, 4, 1), (9, 7, 1), (9, 10, 1), (9, 2, 1), (9, 5, 1), (9, 8, 1),
-                                                              (10, 4, 10), (10, 1, 5), (10, 8, 3), (10, 2, 2), (10, 5, 1), (10, 7, 1), (10, 10, 1), (10, 3, 1), (10, 6, 1), (10, 9, 1)
+                                                              (10, 4, 10), (10, 1, 5), (10, 8, 3), (10, 2, 2), (10, 5, 1), (10, 7, 1), (10, 10, 1), (10, 3, 1), (10, 6, 1), (10, 9, 1);
 
 
 -- BOX
-INSERT INTO BOX (BOX_ID, NAME, BESCHREIBUNG, PREIS, BESTELL_ID) VALUES (1, 'Pasta Box', 'Unsere leckere Pasta Box', 9.99, 11), (2, 'Veggie Box', 'Unsere leckere Veggie Box', 14.99, 12);
+INSERT INTO BOX (BOX_ID, NAME, BESCHREIBUNG, PREIS) VALUES (1, 'Pasta Box', 'Unsere leckere Pasta Box', 9.99), (2, 'Veggie Box', 'Unsere leckere Veggie Box', 14.99);
 
 -- BOX_ZUTAT
 INSERT INTO BOX_ZUTAT (MENGE, BOX_ID, ZUTAT_ID) VALUES (1, 1, 1), (1, 1, 3), (5, 1, 5), (1, 1, 6), (2, 1, 10),
                                                         (4, 2, 8), (5, 2, 5), (4, 2, 4), (5, 2, 10);
+
+-- BESTELLUNG_BOX
+INSERT INTO BESTELLUNG_BOX(BOX_ID, BESTELL_ID, MENGE) VALUES (1, 11, 1),
+                                                            (2, 12, 1);
 
 -- REZEPT
 INSERT INTO REZEPT (REZEPT_ID, NAME, ANLEITUNG) VALUES (1, 'Bolognese', 'Hier könnte Ihr Rezept stehen'), (2, 'Rührei', 'Hier könnte dein Rührei Rezept stehen');
@@ -107,7 +114,7 @@ INSERT INTO BESTELLUNG_REZEPT (BESTELL_ID, REZEPT_ID, MENGE) VALUES (13, 1, 7), 
 -- ERNÄHRUNGSKATEGORIEN
 INSERT INTO ERNÄHRUNGSKATEGORIEN (ERNÄHRUNGSKATEGORIE_ID, NAME, BESCHREIBUNG) VALUES (1, 'Omnivor', 'Die Bezeichnung klingt nur exotisch: Der Omnivor ist ein Allesesser. Der Begriff stammt aus dem Lateinischen – “Omnivore” verbindet die beiden Wörter “omnis” (alles) und “vorare” (fressen). Tierische Produkte kommen bei diesem Ernährungstypen ebenso auf den Speiseplan wie pflanzliche. Bei dieser gemischten Ernährungsweise gilt hier das Motto: Gegessen wird, was schmeckt. Genussmomente sind daher ohne Einschränkungen und Verzicht möglich.'),
                                                                                      (2, 'Vegetarisch', 'Die vegetarische Ernährung ist allen ein Begriff – im Fokus steht dabei der Verzicht auf Fleisch und in der Regel genauso auf Fisch. Es dürfen nur pflanzliche Produkte sowie Produkte von lebenden Tieren verzehrt werden. Das heißt, neben Milch, Käse oder Eiern wird zum Beispiel auch Joghurt oder Butter konsumiert. Übrigens: Vegetarisch zu essen ist keine Erscheinung unserer modernen Zeit und somit kein Trend unter den Ernährungsformen – denn dieser Ernährungstyp hat seine Ursprünge in der Antike.'),
-                                                                                     (3, 'Vegan', 'Keine Produkte tierischen Ursprungs: Das ist für die vegane Ernährungsform das höchste Gebot. Butter und Eier, Käse und Quark, Milch und Kefir – alles tabu. Bei der Auswahl der Lebensmittel gilt es daher, auf eine ausreichende Zufuhr von Vitaminen, Nährstoffen und Mineralien zu achten. Als Ersatz für tierische Zutaten dienen vor allem pflanzliche Alternativen, darunter zum Beispiel milcheiweißfreie Margarine, Sojamilch oder veganer Quark auf Basis von Mandeln oder Kokos. Wichtig zu wissen: Es gibt Lebensabschnitte und -phasen, in denen insgesamt von einer veganen Ernährungsform abgeraten wird: So ist die vegane Küche zum Beispiel für Babys und Kinder sowie in der Schwangerschaft und Stillzeit ungeeignet.')
+                                                                                     (3, 'Vegan', 'Keine Produkte tierischen Ursprungs: Das ist für die vegane Ernährungsform das höchste Gebot. Butter und Eier, Käse und Quark, Milch und Kefir – alles tabu. Bei der Auswahl der Lebensmittel gilt es daher, auf eine ausreichende Zufuhr von Vitaminen, Nährstoffen und Mineralien zu achten. Als Ersatz für tierische Zutaten dienen vor allem pflanzliche Alternativen, darunter zum Beispiel milcheiweißfreie Margarine, Sojamilch oder veganer Quark auf Basis von Mandeln oder Kokos. Wichtig zu wissen: Es gibt Lebensabschnitte und -phasen, in denen insgesamt von einer veganen Ernährungsform abgeraten wird: So ist die vegane Küche zum Beispiel für Babys und Kinder sowie in der Schwangerschaft und Stillzeit ungeeignet.'),
                                                                                     (4, 'Frutar', 'Vor allem Früchte essen? Ja! Das ist der Ansatz der Frutarier, die eine Randgruppe innerhalb der Veganer bilden. Die Pflanzen, Früchte und Nüsse dürfen durch die Ernte zudem nicht beschädigt werden. Daher gibt es bei dieser radikalen Ernährungsform auch Frutarier, die nur Lebensmittel essen, die bereits vom Baum gefallen sind. Mangelerscheinungen vorzubeugen ist bei diesem Ernährungstypen nachweislich sehr schwierig, da die Auswahl verzehrbarer Lebensmittel eng gesteckt ist. Weil zum Beispiel Gemüse wie Möhren oder Kartoffeln geerntet werden müssen, dürfen auch diese nicht auf dem Speiseplan stehen.');                                                       
 
 -- ZUTAT_ERNÄHRUNGSKATEGORIEN
@@ -119,27 +126,34 @@ INSERT INTO ERNÄHRUNGSTRENDS (ERNÄHRUNGSTREND_ID, NAME, BESCHREIBUNG) VALUES (
 -- ZUTAT_ERNÄHRUNGSTRENDS
 INSERT INTO ZUTAT_ERNÄHRUNGSTRENDS (ZUTAT_ID, ERNÄHRUNGSTREND_ID) VALUES (5, 1), (5, 2), (8, 2), (10, 2);
 
+-- HALTUNGSFORM
+INSERT INTO HALTUNGSFORM (HALTUNGSFORM_ID, ART) VALUES (1, 'Haltungsform 1, Stallhaltung'), 
+                                                        (2, 'Haltungsform 2, StallhaltungPlus'),
+                                                        (3, 'Haltungsform 3, Außenklima'),
+                                                        (4, 'Haltungsform 4, Premium'),
+                                                        (5, 'Kleingruppen'),
+                                                        (6, 'Bodenhaltung'),
+                                                        (7, 'Freilandhaltung'),
+                                                        (8, 'Öke-Haltung');
+
 -- EXTRAS
-INSERT INTO EXTRAS (EXTRA_ID, NAME, HALTUNGSFORM_ID) VALUES (1, 'Biosiegel', 0), 
-                                                            (2, 'FairTrade', 0), 
+INSERT INTO EXTRAS (EXTRA_ID, NAME, HALTUNGSFORM_ID) VALUES (1, 'Biosiegel', NULL), 
+                                                            (2, 'FairTrade', NULL), 
                                                             (3, 'Haltungsform', 1), 
                                                             (4, 'Haltungsform', 2), 
                                                             (5, 'Haltungsform', 3), 
-                                                            (6, 'Haltungsform', 4);
-                                                            (7, 'Haltungsform', 5);
-                                                            (8, 'Haltungsform', 6);
-                                                            (9, 'Haltungsform', 7);
+                                                            (6, 'Haltungsform', 4),
+                                                            (7, 'Haltungsform', 5),
+                                                            (8, 'Haltungsform', 6),
+                                                            (9, 'Haltungsform', 7),
                                                             (10, 'Haltungsform', 8);
 
 -- ZUTAT_EXTRAS
 INSERT INTO ZUTAT_EXTRAS (ZUTAT_ID, EXTRA_ID) VALUES (1, 1), 
                                                     (2, 10), 
-                                                    (3, 1);
-                                                    (4, 4);
-                                                    (5, 0);
-                                                    (6, 1);
-                                                    (7, 1);
-                                                    (8, 0);
+                                                    (3, 1),
+                                                    (4, 4),
+                                                    (6, 1),
+                                                    (7, 1),
                                                     (9, 1);
-                                                    (10, 0);
 

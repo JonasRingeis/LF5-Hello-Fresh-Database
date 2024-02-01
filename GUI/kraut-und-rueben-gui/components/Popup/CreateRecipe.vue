d2f2dd23580b21d1869c007fe08c79efe2924a10
tjarkfischer <tjark.fischer@otto.com>
2024-02-01 13:47:43 +0100

    Implementation of Popup for adding recepies.

---
+++ GUI/kraut-und-rueben-gui/components/Popup/CreateRecipe.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="popup-wrapper">
+        <div class="popup">
+            <h2>
+                Create Recipe
+            </h2>
+            <form>
+                <div class="inputfield-wrapper">
+                    <input @input="checkNamePlaceholder" class="inputfield" name="Name" ref="name">
+                    <label for="Name" v-if="namePlaceholder">Name</label>
+                </div>
+                <div class="inputfield-wrapper">
+                    <input @input="checkInstructionsPlaceholder" class="inputfield" name="Instructions" ref="instructions">
+                    <label for="Instructions" v-if="instructionsPlaceholder">Instructions</label>
+                </div>
+                <div class="inputfield-wrapper">
+                    <input @input="checkPreparationTimePlaceholder" class="inputfield" name="Preparation Time"
+                        ref="preparationtime">
+                    <label for="Preparation Time" v-if="preparationTimePlaceholder">Preparation Time</label>
+                </div>
+            </form>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    methods: {
+        checkNamePlaceholder() {
+            this.namePlaceholder = this.$refs.name.value.length == 0;
+        },
+        checkInstructionsPlaceholder() {
+            this.instructionsPlaceholder = this.$refs.instructions.value.length == 0;
+        },
+        checkPreparationTimePlaceholder() {
+            this.preparationTimePlaceholder = this.$refs.preparationtime.value.length == 0;
+        }
+    },
+    data() {
+        return {
+            namePlaceholder: true,
+            instructionsPlaceholder: true,
+            preparationTimePlaceholder: true,
+        }
+    }
+}
+</script>
+<style lang="">
+    
+</style>

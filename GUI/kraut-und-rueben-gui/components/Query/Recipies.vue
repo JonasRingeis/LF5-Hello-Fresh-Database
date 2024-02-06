43af5a6e27cc1c324fa5abeb9049e689aaed3de8
JonasRingeis <jonas.ringeis@otto.de>
2024-02-06 09:50:13 +0100

    Added More Search Options

--- GUI/kraut-und-rueben-gui/components/Query/Recipies.vue
+++ GUI/kraut-und-rueben-gui/components/Query/Recipies.vue
@@ -7,8 +7,10 @@
                 <select @input="setDropdownValue" ref="searchField" class="dropdown">
                     <option value="R.REZEPT_ID">Id</option>
                     <option value="R.NAME">Name</option>
-                    <option value="R.ANLEITUNG">Instruction</option>
+                    <option value="R.DAUER">Preparation Time</option>
                     <option value="R.MENGE">Quantity</option>
+                    <option value="R.ANLEITUNG">Instruction</option>
+                    <option value="Z.BEZEICHNUNG">Ingredient</option>
                 </select>
             </div>
             <div class="selector-wrapper">
